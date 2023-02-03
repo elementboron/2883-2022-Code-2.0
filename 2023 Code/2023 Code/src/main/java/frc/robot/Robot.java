3db9e31d612661de1a3dc4f42f84c6c6fa6b9c64
elementboron <simondanderson47@gmail.com>
2023-02-03 13:49:41 -0600

    Just Driving Functionality
    Attempted to add slow down button

--- 2023 Code/2023 Code/src/main/java/frc/robot/Robot.java
+++ 2023 Code/2023 Code/src/main/java/frc/robot/Robot.java
@@ -15,7 +15,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.LimelightReader;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -29,8 +28,6 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
-
-  private LimelightReader m_Limelight;
 
   /**
    * This function is run when the robot is first started up and should be used for any
