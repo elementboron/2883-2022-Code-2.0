b35e08553ec96f7486b72aaec7667296775b2a0f
elementboron <simondanderson47@gmail.com>
2023-03-04 09:55:00 -0600

    SetUpPathPlanning

---
+++ 2023 Code/2023 Code/src/main/java/frc/robot/autos/PathPlanningAuto.java
@@ -0,0 +1,48 @@
+package frc.robot.autos;
+
+import frc.robot.Constants;
+import frc.robot.commands.FollowTrajectory;
+//import frc.robot.autos.Actions.Action;
+//import frc.robot.autos.Actions.WaitAction;
+import frc.robot.commands.TeleopSwerve;
+import frc.robot.subsystems.Swerve;
+
+import java.util.List;
+
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+//import frc.robot.autos.Actions.WaitAction;
+import frc.robot.subsystems.*;
+
+public class PathPlanningAuto extends SequentialCommandGroup {
+    public PathPlanningAuto(Swerve s_Swerve){
+        TrajectoryConfig config =
+            new TrajectoryConfig(
+                    Constants.AutoConstants.kMaxSpeedMetersPerSecond,
+                    Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+                .setKinematics(Constants.Swerve.swerveKinematics);
+    
+    
+
+
+        addCommands(
+            new InstantCommand((() -> s_Swerve.zeroGyro())),
+            new FollowTrajectory(s_Swerve, traj, true)
+        );
+
+    }
+}
---
+++ 2023 Code/2023 Code/src/main/java/frc/robot/commands/FollowTrajectory.java
@@ -0,0 +1,52 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.pathplanner.lib.PathPlanner;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.CommonMethodExtensions;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.*;
+
+
+public class FollowTrajectory extends CommandBase
+{
+    private final Swerve s_Swerve;
+    private final PathPlannerTrajectory traj;
+    private final boolean isFirstTime;
+    
+    
+
+    public FollowTrajectory(Swerve subsystem, PathPlannerTrajectory traj, boolean isFirstTime)
+    {
+        s_Swerve = subsystem;
+        this.traj = traj;
+        this.isFirstTime = isFirstTime;
+        
+        addRequirements(s_Swerve);
+    }
+
+    @Override
+    public void initialize()
+    {
+        
+    }
+    @Override
+    public void execute() 
+    {  
+        s_Swerve.followTrajectoryCommand(traj, isFirstTime);
+    }
+
+    @Override
+    public boolean isFinished() 
+    {
+        return false;
+    }
+}
